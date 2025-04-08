22c75995feeb9a4b6e50038590c58147e9b61a04
Dudubraga <dudubraga2004@gmail.com>
2025-04-08 03:08:02 -0300

    Initial commit

---
+++ app/(tabs)/SelectOrder.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const SelectOrder = () => {
+  return (
+    <View>
+      <Text>SelectOrder</Text>
+    </View>
+  )
+}
+
+export default SelectOrder
